1de50d0d5660b60b4117ae323d2cd7015028918e
MADEPADIO <madepadio99@gmail.com>
2023-08-08 18:35:48 +0100

    Added database, created the form for components

--- ITTechniciansRepo/Models/KnowledgeBase.cs
+++ ITTechniciansRepo/Models/KnowledgeBase.cs
@@ -4,10 +4,10 @@
     {
         public int Id { get; set; }
         public string Description { get; set; }
-        public string Diagnosis { get; set; }
-        public string Fix { get; set; }
-        public string Category { get; set; }
-        public string Model { get; set; }
-        public string OS { get; set; }
+        public string? Diagnosis { get; set; }
+        public string? Fix { get; set; }
+        public string? Component { get; set; }
+        public string? Model { get; set; }
+        public string? OS { get; set; }
     }
 }
