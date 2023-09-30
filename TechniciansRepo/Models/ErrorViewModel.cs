4ec84498ca99daa1af7a3ce0cab8426ed340781b
MADEPADIO <madepadio99@gmail.com>
2023-09-30 18:42:16 +0100

    Worked on the authentication and views for signed in and non signed in users.

---
+++ TechniciansRepo/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace TechniciansRepo.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
